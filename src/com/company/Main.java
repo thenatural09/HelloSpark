f2f10055045f16cd98499af0366226260cb6c11c
Troy <tward4@tulane.edu>
2016-10-03 12:14:58 -0400

    Adding lists and such

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,23 @@ import spark.ModelAndView;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
+
+    static User user;
+    static ArrayList<User> pastUsers = new ArrayList<>();
 
     public static void main(String[] args) {
         Spark.get(
                 "/",
                 (request, response) -> {
                     HashMap m = new HashMap<>();
-                    m.put("name","Alice");
+                    if (user != null) {
+                        m.put("name",user.name);
+                    }
+                    m.put("pastUsers", pastUsers);
                     return new ModelAndView(m,"home.html");
                 },
                 new MustacheTemplateEngine()
@@ -30,7 +37,19 @@ public class Main {
         Spark.post(
                 "/login",
                 (request,respone) -> {
+                    String name = request.queryParams("loginName");
+                    user = new User(name);
+                    pastUsers.add(user);
                     respone.redirect("/");
+                    return null;
+                }
+        );
+
+        Spark.post(
+                "/logout",
+                (request,response) -> {
+                    user = null;
+                    response.redirect("/");
                     return null;
                 }
         );
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -4,4 +4,9 @@ package com.company;
  * Created by Troy on 10/3/16.
  */
 public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
 }
