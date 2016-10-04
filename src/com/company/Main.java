28c2e193bb5bae83221cf55021ee8a39023cf2f3
Troy <tward4@tulane.edu>
2016-10-04 10:18:36 -0400

    Added sessions and multi user support

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -9,13 +10,17 @@ import java.util.HashMap;
 
 public class Main {
 
-    static User user;
+    static HashMap<String,User> users = new HashMap<>();
     static ArrayList<User> pastUsers = new ArrayList<>();
 
     public static void main(String[] args) {
         Spark.get(
                 "/",
                 (request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("loginName");
+                    User user = users.get(name);
+
                     HashMap m = new HashMap<>();
                     if (user != null) {
                         m.put("name",user.name);
@@ -28,9 +33,7 @@ public class Main {
 
         Spark.get(
                 "/login",
-                (request, response) -> {
-                    return new ModelAndView(null,"login.html");
-                },
+                (request, response) -> new ModelAndView(null,"login.html"),
                 new MustacheTemplateEngine()
         );
 
@@ -38,7 +41,15 @@ public class Main {
                 "/login",
                 (request,respone) -> {
                     String name = request.queryParams("loginName");
-                    user = new User(name);
+                    User user = users.get(name);
+                    if (user == null) {
+                        user = new User(name);
+                        users.put(name,user);
+                    }
+
+                    Session session = request.session();
+                    session.attribute("loginName",name);
+
                     pastUsers.add(user);
                     respone.redirect("/");
                     return null;
@@ -48,7 +59,8 @@ public class Main {
         Spark.post(
                 "/logout",
                 (request,response) -> {
-                    user = null;
+                    Session session = request.session();
+                    session.invalidate();
                     response.redirect("/");
                     return null;
                 }
